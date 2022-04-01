c9d164c992a975fb9713f9c129c5698943080744
RavQs <ravenq1112@gmail.com>
2022-04-02 00:27:27 +0900

    Initial commit

---
+++ src/main/java/com/example/javasrciptlearn/JavaSrciptLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.javasrciptlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSrciptLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSrciptLearnApplication.class, args);
+    }
+
+}
