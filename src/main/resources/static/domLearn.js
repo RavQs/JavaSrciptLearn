3183dc6facc9bc41c197a0e3b2185778cd7b3760
RavQs <ravenq1112@gmail.com>
2022-04-08 23:42:41 +0900

    123

--- src/main/resources/static/domLearn.js
+++ src/main/resources/static/domLearn.js
@@ -4,30 +4,41 @@ const helloH3 = document.getElementById("hello3")
 
 
 setTimeout(() => {
-    addStyles(helloH1,"FUCK HELLO WORLD, WELCOME TO MY  PENGUIN CLUB'S PAGE")
+    addStyles(helloH1, "FUCK HELLO WORLD, WELCOME TO MY  PENGUIN CLUB'S PAGE")
 }, 2500)
 setTimeout(() => {
     addStyles(helloH2, "LET CELEBRATE AND SUCC SOME DICKS")
 }, 4000)
+
+const link = helloH3.querySelector("a")
+
 setTimeout(() => {
-    addStyles(helloH3, "here's ur cute card :)","white","purple")
+    addStyles(link, "here's ur cute card :)", "white", "purple")
 }, 6000)
 
+helloH3.onclick = () => {
+    console.log("link has been used")
+}
 
-function addStyles(node, something, color1 = "red",color2 = "black") {
+
+function addStyles(node, something, color1 = "red", color2 = "black") {
     node.textContent = something
     node.style.backgroundColor = color2
     node.style.color = color1
     node.style.textAlign = "center"
-    node.style.padding = '2rem'
+    node.style.padding = "2rem"
+    node.style.display = "block"
+    node.style.width = "100%"
 }
 
 //События
 
 helloH1.onclick = () => {
     console.log("click")
-    if(helloH1.style.color === "red"){
+    if (helloH1.style.color === "red") {
         helloH1.style.color = "yellow"
+    } else {
+        helloH1.style.color= "red"
     }
 }
 
